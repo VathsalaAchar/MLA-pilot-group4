4560bec87dfd3b16224ac5650896f770c97e5dfc
Ruchika <ruchika5.juneja@gmail.com>
2024-02-25 11:22:44 +0000

    Updated test cases as per the peer review

--- activity-tracking/features/exercise.feature
+++ activity-tracking/features/exercise.feature
@@ -9,18 +9,3 @@ Feature: Exercise Management
     Given I have an exercise
     When I get all exercises
     Then I should see my exercise in the list
-
-  Scenario: Cannot add an exercise with the same name
-    Given I have added an exercise named "Running"
-    When I try to add another exercise named "Running"
-    Then I should see an error message that the exercise already exists
-
-  Scenario: Update an exercise
-    Given I have added an exercise named "Running"
-    When I update the "Running" exercise to last 45 minutes
-    Then the "Running" exercise should show a duration of 45 minutes
-
-  Scenario: Delete an exercise
-    Given I have added an exercise named "Running"
-    When I delete the "Running" exercise
-    Then the "Running" exercise should no longer be in my list
--- activity-tracking/features/step_definitions/exercise_step_definitions.js
+++ activity-tracking/features/step_definitions/exercise_step_definitions.js
@@ -11,24 +11,6 @@ const addExercise = (name, duration) => {
     return { success: true };
 }
 
-const updateExercise = (name, duration) => {
-    const exercise = exercises.find(exercise => exercise.name === name);
-    if (exercise) {
-        exercise.duration = duration;
-        return { success: true };
-    }
-    return { success: false, message: 'Exercise not found' };
-}
-
-const deleteExercise = (name) => {
-    const index = exercises.findIndex(exercise => exercise.name === name);
-    if (index !== -1) {
-        exercises.splice(index, 1);
-        return { success: true };
-    }
-    return { success: false, message: 'Exercise not found' };
-}
-
 const getExercises = () => {
     return exercises;
 }
@@ -61,41 +43,5 @@ Then('the exercise should be added to my list', function () {
     }
     if (exercises.length !== 1) {
         throw new Error("Exercise list should have one exercise");
-    }
-});
-
-Given('I have added an exercise named {string}', function (name) {
-    this.addResult = addExercise(name, 30);
-});
-
-When('I try to add another exercise named {string}', function (name) {
-    this.addResult = addExercise(name, 30);
-});
-
-Then('I should see an error message that the exercise already exists', function () {
-    if (this.addResult.success || this.addResult.message !== 'Exercise already exists') {
-        throw new Error("Expected an error message that the exercise already exists");
-    }
-});
-
-When('I update the {string} exercise to last {int} minutes', function (name, duration) {
-    this.updateResult = updateExercise(name, duration);
-});
-
-Then('the {string} exercise should show a duration of {int} minutes', function (name, duration) {
-    const exercise = exercises.find(exercise => exercise.name === name);
-    if (!exercise || exercise.duration !== duration) {
-        throw new Error(`Expected the ${name} exercise to show a duration of ${duration} minutes`);
-    }
-});
-
-When('I delete the {string} exercise', function (name) {
-    this.deleteResult = deleteExercise(name);
-});
-
-Then('the {string} exercise should no longer be in my list', function (name) {
-    const exercise = exercises.find(exercise => exercise.name === name);
-    if (exercise) {
-        throw new Error(`Expected the ${name} exercise to no longer be in the list`);
     }
 });
