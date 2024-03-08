6ab706958c1e6c5b009830a13d1fcfe51893564e
Vathsala <vathsala.sachar@gmail.com>
2024-03-08 12:38:55 +0000

    updated run to use config

--- analytics/run.py
+++ analytics/run.py
@@ -1,5 +1,8 @@
-from core import app
+from core import create_app
+from config import RunConfig
 
+
+app = create_app(RunConfig)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050)
