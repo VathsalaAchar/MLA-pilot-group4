1edcdd556a9eb101a9b4535b51321df7c21cf4d3
Vathsala <vathsala.sachar@gmail.com>
2024-03-08 12:38:18 +0000

    Using application factory
    - to apply config
    - initialise db
    - register blueprint of stats pages

--- analytics/core/__init__.py
+++ analytics/core/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from core.views import stats_page
+from core.models import mongo
+from flask_cors import CORS
 
 
-app = Flask(__name__)
+def create_app(config):
+    # create and configure app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config)
 
-from core import views
+    mongo.init_app(app)
+
+    CORS(app, resources={r"/*": {"origins": "*"}},
+         methods="GET,HEAD,POST,OPTIONS,PUT,PATCH,DELETE")
+    app.register_blueprint(stats_page)
+
+    return app
