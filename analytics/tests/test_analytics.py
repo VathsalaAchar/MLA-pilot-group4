3a371ab768f437ac76968becede307e58d6217c8
Vathsala <vathsala.sachar@gmail.com>
2024-03-10 13:58:28 +0000

    initial tests to check config and index route
    -- note the tests require mongodb to be up and running

--- analytics/tests/test_analytics.py
+++ analytics/tests/test_analytics.py
@@ -1,2 +1,13 @@
-def test_placeholder():
-    pass
+from core import create_app
+
+
+def test_config():
+    from config import TestConfig
+    assert create_app(TestConfig).testing
+
+
+def test_index_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
