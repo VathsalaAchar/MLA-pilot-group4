eb847ce22ba5e174da9e11f737eb07af05366a2d
Vathsala <vathsala.sachar@gmail.com>
2024-03-12 11:25:11 +0000

    additional tests for index and stats routes

--- analytics/tests/test_analytics.py
+++ analytics/tests/test_analytics.py
@@ -1,4 +1,5 @@
 from core import create_app
+import json
 
 
 def test_config():
@@ -11,3 +12,34 @@ def test_index_route(client):
     assert response.status_code == 200
 
 
+def test_index_route_response_empty_data(client):
+    response = client.get('/')
+    data = json.loads(response.text)
+    # check that the data recieved is empty as mongo fixture is not used
+    assert len(data) == 0
+
+
+def test_index_route_response_data(client, mongo):
+    response = client.get('/')
+    data = json.loads(response.text)
+    # check there are 100 records in the response
+    assert len(data) == 100
+
+
+def test_stats_route(client):
+    response = client.get('/stats')
+    assert response.status_code == 200
+
+
+def test_stats_route_respnse_empty_data(client):
+    response = client.get('/stats')
+    data = json.loads(response.text)
+    # check that the `stats` data recieved is empty as mongo fixture is not used
+    assert len(data['stats']) == 0
+
+
+def test_stats_route_respnse_data(client, mongo):
+    response = client.get('/stats')
+    data = json.loads(response.text)
+    # check that the `stats` data recieved is not empty
+    assert len(data['stats']) > 0
