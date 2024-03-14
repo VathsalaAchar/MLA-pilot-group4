ac05419987cbd8d23be8e5f7f020988eb7fe2812
Vathsala <vathsala.sachar@gmail.com>
2024-03-14 17:00:18 +0000

    updated exercise api to get exercises for a user, sorted by date

--- activity-tracking/routes/exercises.js
+++ activity-tracking/routes/exercises.js
@@ -4,15 +4,17 @@ const Exercise = require('../models/exercise.model');
 
 
 // GET: Retrieve all exercises
-router.get('/', async (req, res) => {
-    try {
-      const exercises = await Exercise.find();
-      res.json(exercises);
-    } catch (error) {
-      res.status(400).json({ error: 'Error: ' + error.message });
-    }
-  });
-  
+router.get('/:username', async (req, res) => {
+  console.log(req.params)
+  try {
+    const exercises = await Exercise.find({ "username": req.params.username }).sort({ date: 'desc' });
+    res.json(exercises);
+    console.log(exercises)
+  } catch (error) {
+    res.status(400).json({ error: 'Error: ' + error.message });
+  }
+});
+
 // POST: Add a new exercise
 router.post('/add', async (req, res) => {
   console.log(req.body)
@@ -64,32 +66,32 @@ router.delete('/:id', async (req, res) => {
 
 // PUT: Update an exercise by ID
 router.put('/update/:id', async (req, res) => {
-    try {
-      const { username, description, duration, date } = req.body;
-  
-      if (!username || !description || !duration || !date) {
-        res.status(400).json({ error: 'All fields are required' });
-        return;
-      }
-  
-      const exercise = await Exercise.findById(req.params.id);
-      if (!exercise) {
-        res.status(404).json({ error: 'Exercise not found' });
-        return;
-      }
-  
-      exercise.username = username;
-      exercise.exerciseType = exerciseType;
-      exercise.description = description;
-      exercise.duration = Number(duration);
-      exercise.date = new Date(date);
-  
-      await exercise.save();
-      res.json({ message: 'Exercise updated!', exercise });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while updating the exercise' });
+  try {
+    const { username, description, duration, date } = req.body;
+
+    if (!username || !description || !duration || !date) {
+      res.status(400).json({ error: 'All fields are required' });
+      return;
     }
-  });
-  
-  module.exports = router;
+
+    const exercise = await Exercise.findById(req.params.id);
+    if (!exercise) {
+      res.status(404).json({ error: 'Exercise not found' });
+      return;
+    }
+
+    exercise.username = username;
+    exercise.exerciseType = exerciseType;
+    exercise.description = description;
+    exercise.duration = Number(duration);
+    exercise.date = new Date(date);
+
+    await exercise.save();
+    res.json({ message: 'Exercise updated!', exercise });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while updating the exercise' });
+  }
+});
+
+module.exports = router;
