da4d23e6e6b98d5736f19e88b8ef8f8e632d7ac5
HarshiSharma <harshi0901@gmail.com>
2024-03-17 20:29:50 +0000

    change to ui using mantine for journal and stats page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 import React from 'react';
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -5,12 +7,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/navbar';
 import TrackExercise from './components/trackExercise';
+import NewStatistics from './components/new_statistics'
+import JournalNew from './components/new_journal'
 import Statistics from './components/statistics';
 import Footer from './components/footer';
 import Login from './components/login';
 import Signup from './components/signup';
 import Journal from './components/journal';
 import logo from './img/CFG_logo.png'; // Update the path to your logo file
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,35 +30,38 @@ function App() {
     setIsLoggedIn(true);
     setCurrentUser(username);
   };
+  
 
   return (
-    <div className="App">
-      <Router>
-        <div className="appTitle">
-          <header>
-            <img src={logo} alt="CFG Fitness App Logo" id="appLogo" />
-            <h1>MLA Fitness App</h1>
-          </header>
-        </div>
+    <MantineProvider>
+      <div className="App">
+        <Router>
+          <div className="appTitle">
+            <header>
+              <img src={logo} alt="CFG Fitness App Logo" id="appLogo" />
+              <h1>MLA Fitness App</h1>
+            </header>
+          </div>
 
-        {isLoggedIn && <NavbarComponent onLogout={handleLogout} />}
+          {isLoggedIn && <NavbarComponent onLogout={handleLogout} />}
 
-        <div className="componentContainer">
-          <Routes>
-            <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
-            <Route path="/signup" element={isLoggedIn ? <Navigate to="/" /> : <Signup onSignup={(username) => {
-              setIsLoggedIn(true);
-              setCurrentUser(username);
-            }} />} />
-            <Route path="/trackExercise" element={isLoggedIn ? <TrackExercise currentUser={currentUser} /> : <Navigate to="/login" />} />
-            <Route path="/statistics" element={isLoggedIn ? <Statistics currentUser={currentUser} /> : <Navigate to="/login" />} />
-            <Route path="/journal" element={isLoggedIn ? <Journal currentUser={currentUser} /> : <Navigate to="/login" />} />
-            <Route path="/" element={isLoggedIn ? <Navigate to="/trackExercise" /> : <Navigate to="/login" />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-    </div>
+          <div className="componentContainer">
+            <Routes>
+              <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
+              <Route path="/signup" element={isLoggedIn ? <Navigate to="/" /> : <Signup onSignup={(username) => {
+                setIsLoggedIn(true);
+                setCurrentUser(username);
+              }} />} />
+              <Route path="/trackExercise" element={isLoggedIn ? <TrackExercise currentUser={currentUser} /> : <Navigate to="/login" />} />
+              <Route path="/statistics" element={isLoggedIn ? <Statistics currentUser={currentUser} /> : <Navigate to="/login" />} />
+              <Route path="/journal" element={isLoggedIn ? <Journal currentUser={currentUser} /> : <Navigate to="/login" />} />
+              <Route path="/" element={isLoggedIn ? <Navigate to="/trackExercise" /> : <Navigate to="/login" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </MantineProvider>
   );
 }
 
--- frontend/src/components/statistics.js
+++ frontend/src/components/statistics.js
@@ -2,6 +2,58 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './statistics.css';
 import config from '../config';
+import { PieChart, Pie, ResponsiveContainer, Sector, Cell, Text } from 'recharts';
+
+const iconMap = {
+  Running: '#322142',
+  Cycling: '#03A6A6',
+  Gym: '#00563E',
+  Swimming: '#CE466B',
+  Other: '#F25757'
+};
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.exerciseType}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <Text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333" width={80} wrap>{`Duration: ${value} min`}</Text>
+    </g>
+  );
+};
 
 const Statistics = ({ currentUser }) => {
   const [data, setData] = useState([]);
@@ -20,21 +72,42 @@ const Statistics = ({ currentUser }) => {
 
   const currentUserData = data.find(item => item.username === currentUser);
 
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="stats-container">
       <h4>Well done, {currentUser}! This is your overall effort:</h4>
-      {currentUserData ? (
-        currentUserData.exercises.map((item, index) => (
-          <div key={index} className="exercise-data">
-            <div><strong>{item.exerciseType}</strong></div>
-            <div>Total Duration: {item.totalDuration} min</div>
-          </div>
-        ))
+      {loading ? (
+        <p>Loading...</p>
+      ) : exercisesData.length > 0 ? (
+        <ResponsiveContainer width="100%" height={400}>
+          <PieChart>
+            <Pie
+              activeIndex={activeIndex}
+              activeShape={renderActiveShape}
+              data={exercisesData}
+              cx="50%"
+              cy="50%"
+              innerRadius={70}
+              outerRadius={120}
+              fill="#8884d8"
+              dataKey="totalDuration"
+              onMouseEnter={onPieEnter}
+            >
+              {exercisesData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={iconMap[entry.exerciseType]} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
       ) : (
         <p>No data available</p>
       )}
     </div>
   );
+
 };
 
 export default Statistics;
